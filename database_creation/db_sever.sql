3950c8d2e82ef9357b9b14ae9f030844d0372f1d
Doriane25 <djdoriane237@gmail.com>
2023-04-04 10:38:24 +0200

    Update db_sever.sql

--- database_creation/db_sever.sql
+++ database_creation/db_sever.sql
@@ -20,52 +20,50 @@ create table `profil`(
     ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1;
 
 
-create table 'post'(
-  'post_id' int(11)unsigned NOT NULL AUTO_INCREMENT,
-  'media' varchar(255) NULL,
-  'created_at' date NOT NULL,
-  'updated_at' date NULL,
-  'user_id' int(11) not NULL,
-  primary key('post_id'),
-  CONSTRAINT fk_users FOREIGN KEY(user_id),
-  REFERENCES users(id),
-)ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1;
+create table `post`(
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `media` varchar(255) NULL,
+  `created_at` date NOT NULL,
+  `updated_at` date NULL,
+  `user_id` int(11) not NULL,
+  primary key(`id`),
+  CONSTRAINT fk_users FOREIGN KEY(user_id)
+  REFERENCES users(id)ON DELETE CASCADE)
+  ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1;
 
-create table 'commentaires'(
-  'comments_id' int(11) not NULL,
-  'content' varchar(70) Not NULL,
-  'created_at' date NOT NULL,
-  'updated_at' date NULL,
-  primary key('comments_id'),
-  CONSTRAINT fk_users FOREIGN KEY(user_id),
-  REFERENCES users(id),
- CONSTRAINT fk_post FOREIGN KEY(post_id),
-  REFERENCES post(id),
-)ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1;
+create table `commentaires`(
+`id` int(11) not NULL,
+`content` varchar(70) Not NULL,
+`created_at` date NOT NULL,
+`updated_at` date NULL,
+`user_id` int(11) NOT NULL,
+`post_id` int(11) NOT NULL,
+ primary key(`id`),
+ /* CONSTRAINT fk_users FOREIGN KEY(user_id)
+  REFERENCES users(id)*/
+CONSTRAINT fk_post FOREIGN KEY(post_id)
+REFERENCES post(id) ON DELETE CASCADE)
+ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1;
 
-
-
-create table 'abonne'(
-  primary key('id_abonne'),
-  CONSTRAINT fk_profil FOREIGN KEY(profil_id),
-  REFERENCES profil(id),
-  'created_at' date NOT NULL,
-)ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1;
-
-
-create table 'messagerie'(
-  'message' int(150),
-  PRIMARY KEY('message_id'),
-  CONSTRAINT fk_users FOREIGN KEY (users_Id),  
-  REFERENCES users(id),
-  CONSTRAINT fk_abonne FOREIGN KEY (abonne_Id),  
-  REFERENCES abonne(id), 
+create table `abonne`(
+`id` int(11) NOT NULL,
+`created_at` date NOT NULL,
+`profil_id` int (11),
+ primary key(`id`),
+ CONSTRAINT fk_profil FOREIGN KEY(profil_id)
+ REFERENCES profil(id) ON DELETE CASCADE
 )ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1;
 
 
-
-
-
-
+create table `messagerie`(
+  `message` varchar(150) not null,
+   `id` int(11) not null,
+   `abonne_id` int (11) not null,
+  PRIMARY KEY(`id`),
+  /*CONSTRAINT fk_users FOREIGN KEY (users_Id),  
+  REFERENCES users(id)*/
+  CONSTRAINT fk_abonne FOREIGN KEY (abonne_id) 
+  REFERENCES abonne(id) ON DELETE CASCADE
+)ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1;
   
   
